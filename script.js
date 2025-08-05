401b3a8e4c8986ef8c729eea396a7eee790d57b0
Lucas <lucasgab.siqueira@gmail.com>
2025-08-04 22:30:02 -0300

    fdsddf

--- script.js
+++ script.js
@@ -66,3 +66,16 @@ window.addEventListener('scroll', () => {
     }
 });
 
+  const container = document.getElementById('teamContainer');
+  const nextBtn = document.getElementById('next');
+  const prevBtn = document.getElementById('prev');
+
+  nextBtn.addEventListener('click', () => {
+    container.scrollBy({ left: 300, behavior: 'smooth' });
+  });
+
+  prevBtn.addEventListener('click', () => {
+    container.scrollBy({ left: -300, behavior: 'smooth' });
+  });
+
+
